85479aa0da5906cb9dbdbaab3cffaf08ea157c00
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2016-03-16 18:55:26 +0100

    fixes

--- getDnbTitel.sh
+++ getDnbTitel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://datendienst.dnb.de/cgi-bin/mabit.pl?cmd=fetch&userID=opendata&pass=opendata&mabheft=DNBTitel.rdf.gz
+wget "http://datendienst.dnb.de/cgi-bin/mabit.pl?cmd=fetch&userID=opendata&pass=opendata&mabheft=DNBTitel.rdf.gz" -O DNBTitel.rdf.gz
 gunzip DNBTitel.rdf.gz
 rm DNBTitel.rdf.gz
 
