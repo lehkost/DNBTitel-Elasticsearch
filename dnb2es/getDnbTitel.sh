cd1e06696b38e58671309ae917ebe622d8015a73
Alex Senger <alex@alexmsenger.de>
2018-05-10 15:21:38 +0200

    #1 update download url

--- dnb2es/getDnbTitel.sh
+++ dnb2es/getDnbTitel.sh
@@ -2,7 +2,7 @@
 
 echo "downloading DNBTitel.rdf.gz"
 
-wget -c --progress=dot:giga "http://datendienst.dnb.de/cgi-bin/mabit.pl?cmd=fetch&userID=opendata&pass=opendata&mabheft=DNBTitel.rdf.gz" -O DNBTitel.rdf.gz
+wget -c --progress=dot:giga "https://data.dnb.de/opendata/DNBTitel.rdf.gz" -O DNBTitel.rdf.gz 
 
 echo "download done, starting unzip"
 
